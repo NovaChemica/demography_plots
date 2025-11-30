db38b93e18b3a1344a6ab63c37d753ca7960e8a4
NovaChemica <adeline.blot@outlook.com>
2025-11-30 23:45:31 +0100

    ajout de export_txt.py, fine_in_txt.py

---
+++ export_txt.py
@@ -0,0 +1,16 @@
+"""Classe utilisable pour représenter des données en .txt"""
+
+class Data:
+    """Représente un ensemble de données"""
+    def __init__(self, data):
+        """Initialisation d'une instance par un jeu de données"""
+        self.data = data
+        
+    def to_txt(self):
+        """tranformation du fichier en fichier txt"""
+        with open(self) as data_object:
+            lines = data_object.readlines()
+            enumerate(colonnes, 1) = lines.split(',')
+        
+
+    
